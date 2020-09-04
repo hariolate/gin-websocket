6310c299246a64312128819e2bf1838af63fac6e
fanghr <chfanghr@gmail.com>
2020-09-05 04:48:31 +0800

    fix: use correct message type

--- cmd/app/service/client.go
+++ cmd/app/service/client.go
@@ -136,7 +136,7 @@ func (c *client) sendMessage(m *protocol.Message) {
 	data, err := proto.Marshal(m)
 	NoError(err)
 	NoError(c.conn.SetWriteDeadline(time.Now().Add(writeWait)))
-	NoError(c.conn.WriteMessage(websocket.TextMessage, data))
+	NoError(c.conn.WriteMessage(websocket.BinaryMessage, data))
 }
 
 func (c *client) sendMessages(ms []*protocol.Message) {
