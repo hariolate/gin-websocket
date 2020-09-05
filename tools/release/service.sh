0d48cccec791ba754e9b8a3aa7780bbf91569616
fanghr <chfanghr@gmail.com>
2020-09-05 08:32:28 +0800

    ci: use right redis_data directory

--- tools/release/service.sh
+++ tools/release/service.sh
@@ -4,7 +4,6 @@ set -e
 
 project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" >/dev/null 2>&1 && pwd)"
 
-mkdir -p ${project_root}/tools/dev/bin
-mkdir -p ${project_root}/tools/dev/db_data
+mkdir -p ${project_root}/tools/release/db_data
 
-docker-compose -f ${project_root}/tools/dev/docker-compose.yml  --project-directory ${project_root} "$@"
+docker-compose -f ${project_root}/tools/release/docker-compose.yml  --project-directory ${project_root} "$@"
